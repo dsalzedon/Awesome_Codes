42534f386d601f760b870094e4bc6593d8e097b1
Daniel Salzedo <66858971+dsalzedon@users.noreply.github.com>
2020-09-24 17:55:50 -0500

    Update card_wars.py
    Use OOP

--- card_wars.py
+++ card_wars.py
@@ -2,36 +2,61 @@ from itertools import product
 from random import shuffle, choice
 
 
+class Cards:
+    suits = ["♠", "♣", "♥", "♦"]
+    values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+    deck = [str(j) + " " + i for i,j in product(suits, values)]
+    shuffle(deck)
+
+
 class War:
 
-    def game():
-        while True:
+    def __init__(self):
+        self.p1 = "Daniel"
+        # self.p1 = input("Type yourname")
+        self.p2 = "Ivan"
+        # self.p2 = input("Type yourname")
 
-            player_1_card = choice(deck)
-            deck.remove(player_1_card)
-            player_2_card = choice(deck)
-            deck.remove(player_2_card)
+        self.p1_wins = 0
+        self.p2_wins = 0
+        self.draws = 0
+
+        print("Welcome player1:\n{} \nWelcome player2:\n{}".format(self.p1, self.p2))
+
+
+    def game(self):
+        while True:
+            player_1_card = choice(Cards.deck)
+            Cards.deck.remove(player_1_card)
+            player_2_card = choice(Cards.deck)
+            Cards.deck.remove(player_2_card)
 
             p1_card = player_1_card.split()
             p2_card = player_2_card.split()
 
-            print("\n{} draws: \n{} \n{} draws: \n{}\n".format(p1, player_1_card, p2, player_2_card))
+            print("\n{} draws: \n{} \n{} draws: \n{}\n".format(self.p1, player_1_card, self.p2, player_2_card))
 
-            if p1_wins > 10:
-                print("Finish, {} wins".format(p1))
+            if self.p1_wins == 10:
+                print("Finish, {} wins".format(self.p1))
+                print(len(Cards.deck))
                 break
-            elif p2_wins > 10:
-                print("Finish, {} wins".format(p2))
+            elif self.p2_wins == 10:
+                print("Finish, {} wins".format(self.p2))
+                print(len(Cards.deck))
                 break
-            elif draws >10:
+            elif self.draws ==10:
                 print("No one wins")
+                print(len(Cards.deck))
             else:
-                p1_value = War.check_char(p1_card[0])
-                p2_value = War.check_char(p2_card[0])
-                print(War.card_battle(p1_value, p2_value))
+                p1_value = self.check_char(p1_card[0])
+                p2_value = self.check_char(p2_card[0])
+                print(self.card_battle(p1_value, p2_value))
 
 
-    def check_char(string):
+    def check_char(self,string):
+
+        self.string = string
+
         if string == "J":
             return 11
         elif string == "Q":
@@ -42,34 +67,21 @@ class War:
             return int(string)
 
 
-    def card_battle(card1, card2):
-        global p1_wins, p2_wins, draws
+    def card_battle(self,card1, card2):
+
+        self.card1 = card1
+        self.card2 = card2
 
         if card1 > card2:
-            p1_wins += 1
+            self.p1_wins += 1
             return "Player 1 wins"
         elif card1 == card2:
-            draws += 1
+            self.draws += 1
             return "Draw"
         else:
-            p2_wins += 1
+            self.p2_wins += 1
             return "Player 2 wins"
 
-
-suits = ["♠", "♣", "♥", "♦"]
-values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
-deck = [str(j) + " " + i for i,j in product(suits, values)]
-shuffle(deck)
-
-p1 = "Daniel"
-# p1 = input("Type yourname")
-p2 = "Ivan"
-# p2 = input("Type yourname")
-
-p1_wins = 0
-p2_wins = 0
-draws = 0
-
-print("Welcome player1:\n{} \nWelcome player2:\n{}".format(p1, p2))
-
-War.game()
+        
+game = War()
+game.game()
