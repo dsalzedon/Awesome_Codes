31ff788bf9a2ae201642ebd0eaf276576a4fbfa3
Daniel Salzedo <66858971+dsalzedon@users.noreply.github.com>
2020-09-26 23:14:12 -0500

    Create test_scripname.py
    Code for remmebering how to test a script

---
+++ test_scripname.py
@@ -0,0 +1,16 @@
+"""
+python -m unittest test_scriptnmae
+"""
+
+import "scriptname"
+import unittest
+
+
+class Test"scriptname"(unittest.TestCase):
+    def test_stage_4(self):
+        self.assertEqual(scripname.function(), True)
+        self.assertEqual(scripname.function(), False)
+
+
+if __name__ == "__main__":
+    unittest.main()
