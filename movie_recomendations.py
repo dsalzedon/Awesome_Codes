b8f8ebe75306c0b2cb356d3807240cb5df343561
Daniel Salzedo <66858971+dsalzedon@users.noreply.github.com>
2020-11-12 16:13:10 -0600

    Update movie_recomendations.py

--- movie_recomendations.py
+++ movie_recomendations.py
@@ -1,5 +1,4 @@
-import requests
-import json
+import requests, json
 from PIL import Image
 
 
@@ -8,11 +7,7 @@ def get_movies_from_tastedive(movie_name):
     url = 'https://tastedive.com/api/similar'
     param_dict = {"q": movie_name, "type": "movies", "limit": 9}
     response = requests.get(url, params=param_dict)
-
-    return response.json()
-
-
-def extract_movie_titles(json_file):
+    json_file = response.json()
     suggestions =  [y['Name'] for y in json_file['Similar']['Results']]
 
     return suggestions
@@ -20,7 +15,7 @@ def extract_movie_titles(json_file):
 
 def get_related_titles(movie_lst):
 
-    multiple_movies  = { y for movie in movie_lst for y in extract_movie_titles(get_movies_from_tastedive(movie)) }
+    multiple_movies  = { y for movie in movie_lst for y in get_movies_from_tastedive(movie) }
     return multiple_movies
 
 
@@ -44,34 +39,31 @@ def get_movie_rating(json_file):
     return user_rating
 
 
+def fix_title(path):
+    words = ""
+    for char in path:
+        if char.isalnum():
+            words = words + str(char)
+        elif char == " ":
+            words = words + " "
+
+    return " ".join(words.split())
+
+
 def get_movie_poster(movie):
     movie_poster = get_movie_data(movie)
     response = requests.get(movie_poster['Poster'])
-    img_path = f"img/{movie_poster['Title']}"
+    title = fix_title(movie_poster['Title'])
+    img_path = f"img/{title}"
     with open(img_path, "wb") as file:
         file.write(response.content)
 
     return img_path
 
 
-def get_sorted_recommendations(movie_lst):
-    movie_recomendations = get_related_titles(movie_lst)
+def make_poster(posters):
 
-    movies_posters = [ get_movie_poster(movie) for movie in movie_recomendations]
-
-    movie_and_ratings= {movie: get_movie_rating(get_movie_data(movie)) for movie in movie_recomendations}
-
-    sorted_movies = [x for x in sorted(movie_and_ratings.keys(), key=lambda k: (movie_and_ratings[k], k), reverse=True)]
-
-    return sorted_movies
-
-
-def get_pic():
-    posters = ["Gladiator", "Inception" , "Terminator 2 Judgment Day", "The Dark Knight Rises", "The Prestige", "V for Vendetta", "Inglourious Basterds", "The Dark Knight", "Shutter Island"]
-
-    # img_path = "img/The Dark Knight Rises"
-
-    all_pictures = [Image.open(r"img/{}".format(x)).convert('RGB') for x in posters]
+    all_pictures = [Image.open(f"{poster}").convert('RGB') for poster in posters]
 
     img_w, img_h = 300, 444
 
@@ -90,14 +82,33 @@ def get_pic():
 
     canvas = canvas.resize((int(canvas.width/2), int(canvas.height/2)))
 
-    canvas.save("img/movie_recomendations2.jpg")
-    canvas.show()
+    canvas.save("img/movie_recomendations.jpg")
+
+    return canvas
+
+
+def get_sorted_recommendations(movie_lst):
+    movie_recomendations = get_related_titles(movie_lst)
+
+    movies_posters = [get_movie_poster(movie) for movie in movie_recomendations]
+
+    # uncommet to get the list of movies order by rating
+    # movie_and_ratings= {movie: get_movie_rating(get_movie_data(movie)) for movie in movie_recomendations}
+    # sorted_movies = [x for x in sorted(movie_and_ratings.keys(), key=lambda k: (movie_and_ratings[k], k), reverse=True)]
+
+    suggestions_poster = make_poster(movies_posters)
+
+    return suggestions_poster
+
 
 
 user_input = "Batman Begins"
 
-# print(get_sorted_recommendations([user_input]))
+movies = get_sorted_recommendations([user_input])
 
-get_pic()
+movies.show()
+
+
+
 
 
