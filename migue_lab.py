38803ac1cc681e8d8d67dfd7307c528870a27a3b
Daniel Salzedo <66858971+dsalzedon@users.noreply.github.com>
2021-07-11 22:17:53 -0500

    Update migue_lab.py

--- migue_lab.py
+++ migue_lab.py
@@ -3,7 +3,6 @@ import time
 
 PORT = "COM5"
 
-
 class AnalogPrinter:
     
     def __init__(self):
@@ -16,60 +15,59 @@ class AnalogPrinter:
 
     def start_gas(self):
         self.board = Arduino(PORT)
-        self.board.analog[0].register_callback(self.myPrintCallback)
+        self.board.analog[0].register_callback(self.sampling_gas)
         self.board.samplingOn(1000 / self.samplingRate)
         self.board.analog[0].enable_reporting()
 
     def start_flama(self):
         self.board = Arduino(PORT)
-        self.board.analog[1].register_callback(self.myPrintCallback_2)
+        self.board.analog[1].register_callback(self.sampling_flama)
         self.board.samplingOn(1000 / self.samplingRate)
         self.board.analog[1].enable_reporting()
 
-
     def start_luz(self):
         self.board = Arduino(PORT)
-        self.board.analog[2].register_callback(self.myPrintCallback_3)
+        self.board.analog[2].register_callback(self.sampling_luz)
         self.board.samplingOn(1000 / self.samplingRate)
         self.board.analog[2].enable_reporting()
 
-    def myPrintCallback(self, data):
+    def sampling_gas(self, data):
         data1 = data * (5 / 1023) * 1000
         self.timestamp += 1 / self.samplingRate
+
         if data1 <= 0.2:
             print(f"{round(self.timestamp, 2)}, {round(data1,2)} El valor del sensor es menor al muestreo",)
             self.board.digital[2].write(0)
-            print("no hay gas")
-            self.gas = 0
+
         else:
-            print(f"{round(self.timestamp, 2)}, {round(data1,2)} El valor del sensor es menor al muestreo",)
+            print(f"{round(self.timestamp, 2)}, {round(data1,2)} El valor del sensor es mayor al muestreo",)
             self.board.digital[2].write(1)
-            print("aqui hubo gas!")
             self.gas = 1
 
-    def myPrintCallback_2(self, data):
+    def sampling_flama(self, data):
         data1 = data * (5 / 1023) * 1000
         self.timestamp += 1 / self.samplingRate
+
         if data1 >= 2:
             print(f"{round(self.timestamp, 2)}, {round(data1,2)} El valor del sensor es mayor al muestreo",)
             self.board.digital[3].write(0)
-            self.flama = 1
-            print("no hay fuego")
+            
+
         else:
             print(f"{round(self.timestamp, 2)}, {round(data1,2)} El valor del sensor es menor al muestreo",)
             self.board.digital[3].write(1)
-            print("fuego!!")
             self.flama = 1
 
-    def myPrintCallback_3(self, data):
+    def sampling_luz(self, data):
         data1 = data * (5 / 1023) * 1000
         self.timestamp += 1 / self.samplingRate
+
         if data1 >= 1:
-            print(f"{round(self.timestamp, 2)}, {round(data1,2)} El valor del sensor es menor al muestreo",)
+            print(f"{round(self.timestamp, 2)}, {round(data1,2)} El valor del sensor es mayor al muestreo",)
             self.board.digital[4].write(0)
 
         else:
-            print(f"{round(self.timestamp, 2)}, {round(data1,2)} El valor del sensor es mayor al muestreo",)
+            print(f"{round(self.timestamp, 2)}, {round(data1,2)} El valor del sensor es menor al muestreo",)
             self.board.digital[4].write(1)
             self.luz = 1
 
@@ -77,44 +75,60 @@ class AnalogPrinter:
         self.board.samplingOff()
         self.board.exit()
 
-analogPrinter = AnalogPrinter()
-
-"""
-print("Let's print data from Arduino's analogue pins for 10secs.")
-
-#  START SENSOR GAS
-analogPrinter.start_gas()
-time.sleep(10)
-analogPrinter.stop()
-
-print("Sensor de gas terminado correctamente")
-
-#  START SENSOR FLAME
-analogPrinter.start_flama()
-time.sleep(10)
-analogPrinter.stop()
-print("Sensor de flama terminado correctamente")
-
-#  START SENSOR LIGHT
-analogPrinter.start_luz()
-time.sleep(10)
-analogPrinter.stop()
-print("Sensor de luminosidad terminado correctamente")
-"""
-
-# CORRER EL SERVO
-
-def start_servo(self):
-        board = Arduino(PORT)
-        pwm = board.get_pin('d:5:p')
-        pwm.write(1)
+    def start_servo(self):
+        self.board = Arduino(PORT)
+        self.pwm = self.board.get_pin('d:5:p')
+        self.pwm.write(0.3)
         time.sleep(5)
-        board.exit()
+        self.pwm.write(0.9)
 
-analogPrinter.luz, analogPrinter.gas, analogPrinter.flama = 1,1,1
 
-if analogPrinter.luz == 1 and analogPrinter.gas == 1 and analogPrinter.flama == 1:
-    print('FUNCIONA')
-    start_servo()
+def main():
+    print("Let's print data from Arduino's analogue pins for 10secs.\n")
+    sensor_gas()
+    sensor_flama()
+    sensor_luz()
+    servo()
+
+
+# START SENSOR GAS
+def sensor_gas():
+    print("Sensor de gas\n")
+    analogPrinter.start_gas()
+    time.sleep(10)
     analogPrinter.stop()
-    print('yo soy la verga')
+    print("Sensor de gas terminado correctamente")
+    print(analogPrinter.gas, analogPrinter.flama, analogPrinter.luz, '\n')
+
+
+# START SENSOR FLAME
+def sensor_flama():
+    print("Sensor de flama\n")
+    analogPrinter.start_flama()
+    time.sleep(10)
+    analogPrinter.stop()
+    print("Sensor de flama terminado correctamente")
+    print(analogPrinter.gas, analogPrinter.flama, analogPrinter.luz, '\n')
+
+
+# START SENSOR LIGHT
+def sensor_luz():
+    print("Sensor de luz\n")
+    analogPrinter.start_luz()
+    time.sleep(10)
+    analogPrinter.stop()
+    print("Sensor de luminosidad terminado correctamente")
+    print(analogPrinter.gas, analogPrinter.flama, analogPrinter.luz, '\n')
+
+
+# Correr Servo
+def servo():
+    if analogPrinter.luz == 1 and analogPrinter.gas == 1 and analogPrinter.flama == 1:
+        analogPrinter.start_servo()
+        analogPrinter.stop()
+        print('yo soy el mas perron aqui')
+
+
+if __name__ == '__main__':
+    analogPrinter = AnalogPrinter()
+    main()
